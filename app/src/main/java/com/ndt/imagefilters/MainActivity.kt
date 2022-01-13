ec33e2c6f30813a7e736088122b3b2367a8e9f27
ndt0409 <vivucenter@gmail.com>
2022-01-13 10:54:36 +0700

    Pick image for editing & filters screen UI

--- app/src/main/java/com/ndt/imagefilters/MainActivity.kt
+++ app/src/main/java/com/ndt/imagefilters/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.ndt.imagefilters
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import com.ndt.imagefilters.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val REQUEST_CODE_PICK_IMAGE = 1
+        const val KEY_IMAGE_URI = "imageUri"
+    }
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        setListener()
+    }
+
+    private fun setListener() {
+        binding.btnViewEditImage.setOnClickListener {
+            Intent(
+                Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+            ).also { pickerIntent ->
+                pickerIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                startActivityForResult(pickerIntent, REQUEST_CODE_PICK_IMAGE)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_PICK_IMAGE && resultCode == RESULT_OK) {
+            data?.data?.let { imageUri ->
+                Intent(applicationContext, EditImageActivity::class.java).also { editImageIntent ->
+                    editImageIntent.putExtra(KEY_IMAGE_URI, imageUri)
+                    startActivity(editImageIntent)
+                }
+            }
+        }
     }
 }
